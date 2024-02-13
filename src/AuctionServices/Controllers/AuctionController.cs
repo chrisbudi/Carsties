e2254e5b6c1245c3887b9d267741f3a826f172bf
chris_budi <chris_budi@outlook.com>
2024-02-13 15:01:13 +0700

    end of section 2

---
+++ src/AuctionServices/Controllers/AuctionController.cs
@@ -0,0 +1,84 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    public class AuctionController(AuctionDbContext context, IMapper mapper) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuctionDto>>> GetAuctions()
+        {
+            var auctions = await context.Auctions
+                .Include(x => x.Item)
+                .OrderBy(x => x.Item.Make)
+                .ToListAsync();
+
+            return Ok(mapper.Map<IEnumerable<AuctionDto>>(auctions));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+
+            var auction = await context.Auctions
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (auction == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<AuctionDto>(auction));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+        {
+            var auction = mapper.Map<Auction>(createAuctionDto);
+            // TODO: add current user
+            auction.Seller = "test";
+
+            context.Auctions.Add(auction);
+            var result = await context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Failed to create auction");
+            return CreatedAtAction(nameof(GetAuctionById),
+                new { auction.Id }, mapper.Map<AuctionDto>(auction));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+        {
+            var auction = await context.Auctions.Include(m => m.Item).FirstOrDefaultAsync(x => x.Id == id);
+            if (auction == null) return NotFound();
+
+            // TODO: check seller is current user
+
+
+            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+            var result = await context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Failed to update auction");
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await context.Auctions.FirstOrDefaultAsync(x => x.Id == id);
+            if (auction == null) return NotFound();
+            context.Remove(auction);
+            var result = await context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("Failed to delete auction");
+            return Ok();
+        }
+
+    }
+}
---
+++ src/AuctionServices/Program.cs
@@ -0,0 +1,49 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+// add npgsql connection
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+
+var conn = $"Connection string: {builder.Configuration.GetConnectionString("DefaultConnection")}";
+Debug.WriteLine(conn);
+Console.WriteLine(conn);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    try
+    {
+        DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Debug.WriteLine(ex.Message);
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

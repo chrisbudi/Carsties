496f7ba51466750b5c44569c1f2f057cbdfd0586
chris_budi <chris_budi@outlook.com>
2024-03-07 17:59:00 +0700

    add identity service and change some of auction services

--- AuctionServices/Controllers/AuctionController.cs
+++ AuctionServices/Controllers/AuctionController.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Contracts;
 using MassTransit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -45,12 +46,14 @@ namespace AuctionService.Controllers
             return Ok(_mapper.Map<AuctionDto>(auction));
         }
 
+
+        [Authorize]
         [HttpPost]
         public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
         {
             var auction = _mapper.Map<Auction>(createAuctionDto);
-            // TODO: add current user
-            auction.Seller = "test";
+
+            auction.Seller = User.Identity.Name;
 
             _context.Auctions.Add(auction);
 
@@ -69,14 +72,15 @@ namespace AuctionService.Controllers
                 new { auction.Id }, newAuction);
         }
 
+        [Authorize]
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
         {
             var auction = await _context.Auctions.Include(m => m.Item).FirstOrDefaultAsync(x => x.Id == id);
             if (auction == null) return NotFound();
 
-            // TODO: check seller is current user
 
+            if (auction.Seller != User.Identity.Name) return Forbid();
 
             auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
             auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
@@ -96,7 +100,11 @@ namespace AuctionService.Controllers
         {
             var auction = await _context.Auctions.FirstOrDefaultAsync(x => x.Id == id);
             if (auction == null) return NotFound();
+
+            if (auction.Seller != User.Identity.Name) return Forbid();
+
             _context.Remove(auction);
+
 
             await _publish.Publish(_mapper.Map<AuctionDeleted>(new { Id = auction.Id }));
 
--- AuctionServices/Program.cs
+++ AuctionServices/Program.cs
@@ -1,6 +1,7 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
@@ -41,6 +42,15 @@ builder.Services.AddMassTransit(x =>
 });
 
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 
@@ -58,6 +68,7 @@ if (app.Environment.IsDevelopment())
     }
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
